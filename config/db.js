f8c5f7c4ab362f2ac1349a70e21df8d75b9a7f00
janand10 <janand10@my.centennialcollege.ca>
2021-12-11 22:49:12 -0500

    Initial Commit

---
+++ config/db.js
@@ -0,0 +1,7 @@
+/*COMP 229-007 Midterm
+Author: Jashanpreet Singh Anand
+ID: 301168003*/
+
+module.exports = {
+    "AtlasDB": "mongodb+srv://JashanpreetSinghAnand:Anand8400@midterm.ryjq8.mongodb.net/books229?retryWrites=true&w=majority"
+}
---
+++ routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+let controlerIndex = require('../controllers/index');
+
+/* GET home page. */
+router.get('/', controlerIndex.home);
+
+router.get('/home',controlerIndex.home);
+
+module.exports = router;
